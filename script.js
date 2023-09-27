b1dc9381c7296a4462b76f52616f802cfd9bce0b
manasa <manasapalle528@gmail.com>
2023-09-27 18:20:36 +0530

    thanks page added

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function beforesubmit(){
     let inputdate = document.querySelector(".inputdate");
     console.log("inputdate.value",inputdate.value);
     let formattedDate= new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value=formattedDate;
+    outputdate.value = formattedDate;
 }
