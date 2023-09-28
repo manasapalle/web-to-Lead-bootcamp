c566e64bf85e42c442e1fc89c659d2b69e0daa43
manasa <manasapalle528@gmail.com>
2023-09-28 19:21:36 +0530

    captcha validation added

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-function beforesubmit(){
+let captchachecked=false;
+function beforesubmit(event){
+   if(captchachecked){
+    
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate");
     console.log("inputdate.value",inputdate.value);
     let formattedDate= new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value = formattedDate;
+   }else
+   {
+      alert("please check the recaptcha box to submit the lead");
+      event.preventDefault();
+   }
 }
-function timestamp() { 
+
+ function timestamp() { 
     var response = document.getElementById("g-recaptcha-response"); 
-    if (response == null || response.value.trim() == "")
-     {
-        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());
-       document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
-       
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems);
     } 
- 
-} setInterval(timestamp, 500);
+    
+} setInterval(timestamp, 500); 
+
+function captchasuccess(){
+    captchachecked=true;
+}
